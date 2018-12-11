b911517af8db5b7f513230b5bedbe2fddfe2057d
espen <espen.volnes@gmail.com>
2018-12-11 10:51:33 +0100

    style fixes

--- analysis/scripts/decompress_trace/decompress-trace.py
+++ analysis/scripts/decompress_trace/decompress-trace.py
@@ -1,188 +1,151 @@
 
 import os
-
 from pathlib import Path
+
 from kivy.app import App
 from kivy.uix.boxlayout import BoxLayout
-from kivy.uix.togglebutton import ToggleButton
 from kivy.uix.button import Button
 from kivy.uix.label import Label
 from kivy.uix.popup import Popup
+from kivy.uix.togglebutton import ToggleButton
 
-# FEMs
-# - Enqueue packet on cc2420::rx. Check if currently overflow on chip, return if so. Check for overflow, and set flag if so.
-#   Start HIRQ-321
-# - CALLBACK from readDoneLength
-# - CALLBACK from readDoneFcf
-# - CALLBACK from readDoneAckLength
-# - CALLBACK from sendTask (if successfull) which enqueues an ack, and schedules HIRQ-321 if no thread is currently reading packets.
-
-# Thread 0: task loop and tasks
-# Thread 321: InterruptFIFOP.fired, PEUSTART readDoneLength.
-# PEUSTART will be followed eventually by a call to a HIRQ, which is readDoneLength in the above case. In a signature,
-# the PEUSTART line will include the number of cycles it takes before the HIRQ is called, and the normal. That way,
-# it can be modelled accurately when a HIRQ is called.
-# Thread 1234: readDoneLength - CALLBACK to schedule readDoneFcf
-# Thread 2345: readDoneFcf - CALLBACK to scheule readDonePayload
-# Thread 3456: readDonePayload - ENQUEUE SRVQUEUE softirq::rx receiveDone_task
-# Thread 4567: readDoneAckLength - CALLBACK to schedule readDoneAckPayload
-# Thread 5678: readDoneAckPayload
+# FSMs
 
 # Services (tasks):
-# - task_loop - DEQUEUE SRVQUEUE softirq::rx
-# - receiveDone_task - ENQUEUE SRVQUEUE softirq::rx sendTask, ENQUEUE PKTQUEUE ip::sendinfo, ENQUEUE PKTQUEUE ip::sendpacket
-# - sendTask - QUEUECOND to decide whether to drop packet or not, DEQUEUE PKTQUEUE ip::sendinfo, DEQUEUE PKTQUEUE ip::sendpacket, ENQUEUE PKTQUEUE cc2420::tx
-# - waitForNextPacket - When finished receiving a packet in readDonePayload or readDoneAckPayload, we call this service.
-#   The service can be initialized from a callback function in the ns3 model. For instance, IF cc2420::rxfifo.size > 0, THEN CALL waitForNextPacket.
-#   Alternatively, the two HIRQs can end with QUEUECOND cc2420::rx, which causes waitForNextPacket to be called if size > 0, or not if size == 0.
 
 # Queues:
-# - ip::sendinfo
-# - ip::sendpacket
-# - softirq::rx
-# - cc2420::rx
-# - cc2420::tx
-
-# Thread 0 includes all scheduler / tasks. Since these can be interrupted and currently we don't know of a place
-# in code where this can be traced, we prepend all event traces in thread 0 with "CTXSW 0 0 [CPU_CYCLES] 0 0 0 0 s". This
-# means that no matter where we're interrupted in thread 0, we will still be able to switch back to thread 0 for the events.
 
 trace_id_to_CSEW_events = {
-	'0': "HIRQENTRY 0 1 [CPU_CYCLES] 1 0 0 interruptfifop_fired s\n",
+	'0': 	"HIRQENTRY 0 1 [CPU_CYCLES] 1 0 0 interruptfifop_fired s\n",
 
-	'1': "TEMPSYNCH 0 1 [CPU_CYCLES] 1 0 1 (TEMP) readDoneLength\n" + \
-		"PEUSTART 0 1 [CPU_CYCLES] 1 2 0 (TEMP) s\n" + \
-		"WAITCOMPL 0 1 [CPU_CYCLES] 1 0 1 (TEMP) readDoneLength\n",
+	'1': 	"TEMPSYNCH 0 1 [CPU_CYCLES] 1 0 1 (TEMP) readDoneLength\n" +
+			"PEUSTART 0 1 [CPU_CYCLES] 1 2 0 (TEMP) s\n" +
+			"WAITCOMPL 0 1 [CPU_CYCLES] 1 0 1 (TEMP) readDoneLength\n",
 
-	'2': "TEMPSYNCH 0 1 [CPU_CYCLES] 1 0 1 (TEMP) readDoneLength\n" + \
-		"PEUSTART 0 1 [CPU_CYCLES] 1 5 0 (TEMP) s\n" + \
-		"WAITCOMPL 0 1 [CPU_CYCLES] 1 0 1 (TEMP) readDoneLength\n",
+	'2': 	"TEMPSYNCH 0 1 [CPU_CYCLES] 1 0 1 (TEMP) readDoneLength\n" +
+			"PEUSTART 0 1 [CPU_CYCLES] 1 5 0 (TEMP) s\n" +
+			"WAITCOMPL 0 1 [CPU_CYCLES] 1 0 1 (TEMP) readDoneLength\n",
 
-	'3': "HIRQEXIT 0 1 [CPU_CYCLES] 1 0 0 interruptfifop_fired s\n",
+	'3': 	"HIRQEXIT 0 1 [CPU_CYCLES] 1 0 0 interruptfifop_fired s\n",
 
-	'4': "HIRQENTRY 0 2 [CPU_CYCLES] 2 0 0 readDoneLength s\n" + \
-		"COMPL 0 2 [CPU_CYCLES] 1 0 0 (TEMP) readDoneLength\n",
+	'4': 	"HIRQENTRY 0 2 [CPU_CYCLES] 2 0 0 readDoneLength s\n" +
+			"COMPL 0 2 [CPU_CYCLES] 1 0 0 (TEMP) readDoneLength\n",
 
-	'5': "TEMPSYNCH 0 2 [CPU_CYCLES] 2 0 1 (TEMP) readDoneFcf\n" + \
-		"PEUSTART 0 2 [CPU_CYCLES] 2 3 0 (TEMP) s\n" + \
-		"WAITCOMPL 0 2 [CPU_CYCLES] 2 0 1 (TEMP) readDoneFcf\n",
+	'5': 	"TEMPSYNCH 0 2 [CPU_CYCLES] 2 0 1 (TEMP) readDoneFcf\n" +
+			"PEUSTART 0 2 [CPU_CYCLES] 2 3 0 (TEMP) s\n" +
+			"WAITCOMPL 0 2 [CPU_CYCLES] 2 0 1 (TEMP) readDoneFcf\n",
 
-	'6': "HIRQEXIT 0 2 [CPU_CYCLES] 2 0 0 readDoneLength s\n",
+	'6': 	"HIRQEXIT 0 2 [CPU_CYCLES] 2 0 0 readDoneLength s\n",
 
-	'7': "HIRQENTRY 0 3 [CPU_CYCLES] 3 0 0 readDoneFcf s\n" + \
-		"COMPL 0 3 [CPU_CYCLES] 1 0 0 (TEMP) readDoneFcf\n",
+	'7': 	"HIRQENTRY 0 3 [CPU_CYCLES] 3 0 0 readDoneFcf s\n" +
+			"COMPL 0 3 [CPU_CYCLES] 1 0 0 (TEMP) readDoneFcf\n",
 
-	'8': "TEMPSYNCH 0 3 [CPU_CYCLES] 3 0 1 (TEMP) readDonePayload\n" + \
-		"PEUSTART 0 3 [CPU_CYCLES] 3 4 0 (TEMP) s\n" + \
-		"WAITCOMPL 0 3 [CPU_CYCLES] 3 0 1 (TEMP) readDonePayload\n",
+	'8': 	"TEMPSYNCH 0 3 [CPU_CYCLES] 3 0 1 (TEMP) readDonePayload\n" +
+			"PEUSTART 0 3 [CPU_CYCLES] 3 4 0 (TEMP) s\n" +
+			"WAITCOMPL 0 3 [CPU_CYCLES] 3 0 1 (TEMP) readDonePayload\n",
 
-	'9': "HIRQEXIT 0 3 [CPU_CYCLES] 3 0 0 readDoneFcf s\n",
+	'9': 	"HIRQEXIT 0 3 [CPU_CYCLES] 3 0 0 readDoneFcf s\n",
 
-	'10': "HIRQENTRY 0 4 [CPU_CYCLES] 4 0 0 readDonePayload s\n" + \
-		"COMPL 0 4 [CPU_CYCLES] 1 0 0 (TEMP) readDonePayload\n",
+	'10': 	"HIRQENTRY 0 4 [CPU_CYCLES] 4 0 0 readDonePayload s\n" +
+			"COMPL 0 4 [CPU_CYCLES] 1 0 0 (TEMP) readDonePayload\n",
 
-	'11': "SRVQUEUE 0 4 [CPU_CYCLES] 0 2 0 receiveDone_task 0\n" + \
-		"HIRQEXIT 0 4 [CPU_CYCLES] 4 0 0 readDonePayload s\n",
+	'11':	"SRVQUEUE 0 4 [CPU_CYCLES] 0 2 0 receiveDone_task 0\n" +
+			"HIRQEXIT 0 4 [CPU_CYCLES] 4 0 0 readDonePayload s\n",
 
-	# e39 means that we don't have to record the start of each service / task in each service, but let the scheduler record this event.
-	'12': "",# "SRVQUEUE 0 0 [CPU_CYCLES] 1 2 0 receiveDone_task s\n"
+	'12': "",  # "SRVQUEUE 0 0 [CPU_CYCLES] 1 2 0 receiveDone_task s\n"
 
-	'13': "QUEUECOND 0 0 [CPU_CYCLES] 0 0 0 receiveDone_task s\n",
+	'13': 	"QUEUECOND 0 0 [CPU_CYCLES] 0 0 0 receiveDone_task s\n",
 
-	'14': "QUEUECOND 0 0 [CPU_CYCLES] 0 0 0 receiveDone_task s\n",
+	'14': 	"QUEUECOND 0 0 [CPU_CYCLES] 0 0 0 receiveDone_task s\n",
 
-	'15': "PKTQUEUE 0 0 [CPU_CYCLES] 0 1 0 receiveDone_task s\n",
+	'15': 	"PKTQUEUE 0 0 [CPU_CYCLES] 0 1 0 receiveDone_task s\n",
 
-	'41': "SRVQUEUE 0 0 [CPU_CYCLES] 0 2 0 sendTask 0\n",
+	'41': 	"SRVQUEUE 0 0 [CPU_CYCLES] 0 2 0 sendTask 0\n",
 
-	'16': "",# "SRVEXIT 0 0 [CPU_CYCLES] 0 0 0 receiveDone_task s\n"
+	'16': 	"",  # "SRVEXIT 0 0 [CPU_CYCLES] 0 0 0 receiveDone_task s\n"
 
-	# e39 means that we don't have to record the start of each service / task in each service, but let the scheduler record this event.
-	'17': "",# "SRVQUEUE 0 0 [CPU_CYCLES] 1 2 0 sendTask s\n"
+	'17': 	"",  # "SRVQUEUE 0 0 [CPU_CYCLES] 1 2 0 sendTask s\n"
 
-	'18': "QUEUECOND 0 0 [CPU_CYCLES] 0 0 0 sendTask s\n",
+	'18': 	"QUEUECOND 0 0 [CPU_CYCLES] 0 0 0 sendTask s\n",
 
-	'19': "QUEUECOND 0 0 [CPU_CYCLES] 0 0 0 sendTask s\n",
+	'19': 	"QUEUECOND 0 0 [CPU_CYCLES] 0 0 0 sendTask s\n",
 
-	'20': "PKTQUEUE 0 0 [CPU_CYCLES] 1 1 0 sendTask s\n",
+	'20': 	"PKTQUEUE 0 0 [CPU_CYCLES] 1 1 0 sendTask s\n",
 
 	# (TEMP) was 2_synch
-	'21': "TEMPSYNCH 0 0 [CPU_CYCLES] 0 0 1 (TEMP) sendDone\n" + \
-		"PEUSTART 0 0 [CPU_CYCLES] 0 7 0 (TEMP) s\n" + \
-		"WAITCOMPL 0 0 [CPU_CYCLES] 0 0 1 (TEMP) sendDone\n",
+	'21': 	"TEMPSYNCH 0 0 [CPU_CYCLES] 0 0 1 (TEMP) sendDone\n" +
+			"PEUSTART 0 0 [CPU_CYCLES] 0 7 0 (TEMP) s\n" +
+			"WAITCOMPL 0 0 [CPU_CYCLES] 0 0 1 (TEMP) sendDone\n",
 
-	'22': "", # "SRVEXIT 0 0 [CPU_CYCLES] 0 0 0 sendTask s\n"
+	'22': "",  # "SRVEXIT 0 0 [CPU_CYCLES] 0 0 0 sendTask s\n"
 
 	# (TEMP) was 2_synch
-	'23': "HIRQENTRY 0 7 [CPU_CYCLES] 7 0 0 sendDone s\n" + \
-		"COMPL 0 7 [CPU_CYCLES] 1 0 0 (TEMP) sendDone\n" + \
-		"SRVQUEUE 0 7 [CPU_CYCLES] 0 2 0 sendDone_task s\n" + \
-		"HIRQEXIT 0 7 [CPU_CYCLES] 7 0 0 sendDone s\n",
+	'23': 	"HIRQENTRY 0 7 [CPU_CYCLES] 7 0 0 sendDone s\n" +
+			"COMPL 0 7 [CPU_CYCLES] 1 0 0 (TEMP) sendDone\n" +
+			"SRVQUEUE 0 7 [CPU_CYCLES] 0 2 0 sendDone_task s\n" +
+			"HIRQEXIT 0 7 [CPU_CYCLES] 7 0 0 sendDone s\n",
 
-	'24': "HIRQENTRY 0 5 [CPU_CYCLES] 5 0 0 readDoneAckLength s\n" + \
-		"COMPL 0 5 [CPU_CYCLES] 1 0 0 (TEMP) readDoneAckLength\n",
+	'24': 	"HIRQENTRY 0 5 [CPU_CYCLES] 5 0 0 readDoneAckLength s\n" +
+			"COMPL 0 5 [CPU_CYCLES] 1 0 0 (TEMP) readDoneAckLength\n",
 
-	'25': "TEMPSYNCH 0 5 [CPU_CYCLES] 1 0 1 (TEMP) readDoneAck\n" + \
-		"PEUSTART 0 5 [CPU_CYCLES] 5 6 0 (TEMP) s\n" + \
-		"WAITCOMPL 0 5 [CPU_CYCLES] 1 0 1 (TEMP) readDoneAckLength\n",
+	'25': 	"TEMPSYNCH 0 5 [CPU_CYCLES] 1 0 1 (TEMP) readDoneAck\n" +
+			"PEUSTART 0 5 [CPU_CYCLES] 5 6 0 (TEMP) s\n" +
+			"WAITCOMPL 0 5 [CPU_CYCLES] 1 0 1 (TEMP) readDoneAckLength\n",
 
-	'26': "HIRQEXIT 0 5 [CPU_CYCLES] 5 0 0 readDoneAckLength s\n",
+	'26': 	"HIRQEXIT 0 5 [CPU_CYCLES] 5 0 0 readDoneAckLength s\n",
 
-	'27': "",
+	'27': 	"",
 
-	'28': "",# "HIRQEXIT 0 0 [CPU_CYCLES] 6 0 0 readDoneAckLength s\n"
+	'28': 	"",  # "HIRQEXIT 0 0 [CPU_CYCLES] 6 0 0 readDoneAckLength s\n"
 
-	'29': "HIRQENTRY 0 0 [CPU_CYCLES] 6 0 0 readDoneAckPayload s\n" + \
-		"COMPL 0 6 [CPU_CYCLES] 1 0 0 (TEMP) readDoneAckPayload\n",
+	'29': 	"HIRQENTRY 0 0 [CPU_CYCLES] 6 0 0 readDoneAckPayload s\n" +
+			"COMPL 0 6 [CPU_CYCLES] 1 0 0 (TEMP) readDoneAckPayload\n",
 
-	'30': "HIRQEXIT 0 6 [CPU_CYCLES] 6 0 0 readDoneAckPayload s\n",
+	'30': 	"HIRQEXIT 0 6 [CPU_CYCLES] 6 0 0 readDoneAckPayload s\n",
 
-	# e39 means that we don't have to record the start of each service / task in each service, but let the scheduler record this event.
-	'31': "",# "SRVQUEUE 0 0 [CPU_CYCLES] 1 2 0 sendDone_task s\n"
+	'31': 	"",  # "SRVQUEUE 0 0 [CPU_CYCLES] 1 2 0 sendDone_task s\n"
 
-	'32': "",# "SRVEXIT 0 0 [CPU_CYCLES] 0 0 0 sendDone_task s\n"
+	'32': 	"",  # "SRVEXIT 0 0 [CPU_CYCLES] 0 0 0 sendDone_task s\n"
 
-	'33': "CTXSW 0 0 [CPU_CYCLES] 0 0 0 0 s\n",
+	'33': 	"CTXSW 0 0 [CPU_CYCLES] 0 0 0 0 s\n",
 	# "SRVENTRY 0 0 [CPU_CYCLES] 0 0 0 scheduler_tasks s\n"# +
-	# "SRVENTRY 0 0 [CPU_CYCLES] 0 0 0 scheduler_tasks s\n" + \
+	# "SRVENTRY 0 0 [CPU_CYCLES] 0 0 0 scheduler_tasks s\n" +
 	# "LOOPSTART 0 0 [CPU_CYCLES] 0 0 0 2 task_loop\n"
 
-	'34': "",# "CTXSW 0 0 [CPU_CYCLES] 0 0 0 0 s\n" + "QUEUECOND 0 0 [CPU_CYCLES] 0 0 0 task_loop s\n"
+	'34': 	"",  # "CTXSW 0 0 [CPU_CYCLES] 0 0 0 0 s\n" + "QUEUECOND 0 0 [CPU_CYCLES] 0 0 0 task_loop s\n"
 
-	'35': "",# "QUEUECOND 0 0 [CPU_CYCLES] 0 0 0 task_loop s\n" + \
+	'35': 	"",  # "QUEUECOND 0 0 [CPU_CYCLES] 0 0 0 task_loop s\n" +
 	# "LOOPSTART 0 0 [CPU_CYCLES] 0 0 0 4 s\n"
 
-	'36': "",# "TTWAKEUP 0 0 [CPU_CYCLES] 0 0 0 task_loop_wait s\n" + \
-	# "QUEUECOND 0 0 [CPU_CYCLES] 0 0 0 task_loop_wait s\n" + \
+	'36': 	"",  # "TTWAKEUP 0 0 [CPU_CYCLES] 0 0 0 task_loop_wait s\n" +
+	# "QUEUECOND 0 0 [CPU_CYCLES] 0 0 0 task_loop_wait s\n" +
 	# "LOOPRSTART 0 0 [CPU_CYCLES] 0 0 0 4 s\n"
 
-	'37': "",# "TTWAKEUP 0 0 [CPU_CYCLES] 0 0 0 task_loop_wait s\n" + \
-	# "QUEUECOND 0 0 [CPU_CYCLES] 0 0 0 task_loop_wait s\n" + \
+	'37': 	"",  # "TTWAKEUP 0 0 [CPU_CYCLES] 0 0 0 task_loop_wait s\n" +
+	# "QUEUECOND 0 0 [CPU_CYCLES] 0 0 0 task_loop_wait s\n" +
 	# "LOOPSTOP 0 0 [CPU_CYCLES] 0 0 0 4 s\n"
 
-	# Need to find a way for task loop to initiate task. SRVQUEUE requires name of service. How do we get the service name here?
-	'38': "SRVEXIT 0 0 [CPU_CYCLES] 0 0 0 SERVICE_PLACEHOLDER s\n" + "LOOPRSTART 0 0 [CPU_CYCLES] 0 0 0 2 task_loop\n",
+	'38': 	"SRVEXIT 0 0 [CPU_CYCLES] 0 0 0 SERVICE_PLACEHOLDER s\n" + "LOOPRSTART 0 0 [CPU_CYCLES] 0 0 0 2 task_loop\n",
 
-	# The 1 in this trace means that we are dequeueing a service. The 2 means that we are dequeueing from queue 2, which is softirq::rx.
-	'39': "SRVQUEUE 0 0 [CPU_CYCLES] 1 2 0 SERVICE_PLACEHOLDER s\n",
+	'39': 	"SRVQUEUE 0 0 [CPU_CYCLES] 1 2 0 SERVICE_PLACEHOLDER s\n",
 
-	'40': "PKTQUEUE 0 0 [CPU_CYCLES] 0 3 0 sendTask s\n",
+	'40': 	"PKTQUEUE 0 0 [CPU_CYCLES] 0 3 0 sendTask s\n",
 
-	'42': "",
+	'42': 	"",
 
-	'43': "",
+	'43': 	"",
 
-	'44': "",
+	'44': 	"",
 
-	'45': "",
+	'45': 	"",
 
-	'46': "",# "SRVEXIT 0 0 [CPU_CYCLES] 0 0 0 sendDone_task s\n"
+	'46': 	"",  # "SRVEXIT 0 0 [CPU_CYCLES] 0 0 0 sendDone_task s\n"
 
-	'47':  "",
+	'47':  	"",
 
-	'71': "QUEUECOND 0 0 [CPU_CYCLES] 0 0 0 sendTask empty\n",
+	'71': 	"QUEUECOND 0 0 [CPU_CYCLES] 0 0 0 sendTask empty\n",
 
-	'91': "QUEUECOND 0 0 [CPU_CYCLES] 0 0 0 sendTask notempty\n"
+	'91': 	"QUEUECOND 0 0 [CPU_CYCLES] 0 0 0 sendTask notempty\n"
 }
 
 printed = False
@@ -199,8 +162,7 @@ class TestApp(App):
 	def __init__(self):
 		super(TestApp, self).__init__()
 		content = Button(text='Success')
-		self.popup = Popup(title='Decompression finished', content=content,
-						   auto_dismiss=True)
+		self.popup = Popup(title='Decompression finished', content=content)
 		content.bind(on_press=self.popup.dismiss)
 		self.bl = BoxLayout(orientation='vertical')
 
@@ -231,9 +193,9 @@ class TestApp(App):
 
 	def build(self):
 		self.bl.add_widget(Label(text='Select trace file to decompress to CSEM events'))
-		pathlist = [(os.stat('../../traces/'+p.name).st_mtime, p.name) for p in Path('../../traces').glob('**/*.trace')]
-		pathlist.sort(key=lambda s: s[0])
-		for i, (time, fn) in enumerate(pathlist):
+		path_list = [(os.stat('../../traces/'+p.name).st_mtime, p.name) for p in Path('../../traces').glob('**/*.trace')]
+		path_list.sort(key=lambda s: s[0])
+		for i, (time, fn) in enumerate(path_list):
 			if i == 0:
 				self.bl.add_widget(ToggleButton(text=fn, group="trace file", state='down'))
 			else:
--- analysis/scripts/trace_analysis/trace-analysis.py
+++ analysis/scripts/trace_analysis/trace-analysis.py
@@ -1,56 +1,56 @@
 
-import re
-import os
 import errno
+import os
+import re
 from pathlib import Path
+
+import numpy as np
+import numpy_indexed as npi
+import seaborn as sns
 from kivy.app import App
 from kivy.uix.boxlayout import BoxLayout
-from kivy.uix.togglebutton import ToggleButton
 from kivy.uix.button import Button
 from kivy.uix.label import Label
 from kivy.uix.popup import Popup
-
+from kivy.uix.togglebutton import ToggleButton
+from matplotlib import pyplot as plt
 from openpyxl_templates import TemplatedWorkbook
 from openpyxl_templates.table_sheet import TableSheet
 from openpyxl_templates.table_sheet.columns import CharColumn, IntColumn
-import numpy as np
-import numpy_indexed as npi
-from matplotlib import pyplot as plt
-import seaborn as sns
 
 trace_fn = None
 
-FIRST_TRACEID = 1
+FIRST_trace_id = 1
 
 np.set_printoptions(edgeitems=30, linewidth=100000,
                     formatter=dict(float=lambda x: "%.3g" % x))
 
 
 class TraceSheet(TableSheet):
-    traceId = CharColumn()
-    cpuId = CharColumn()
-    threadId = CharColumn()
+    trace_id = CharColumn()
+    cpu_id = CharColumn()
+    thread_id = CharColumn()
     timestamp = CharColumn()
 
 
-class TraceIdheet(TableSheet):
-    traceIdFrom = CharColumn()
-    traceIdTo = CharColumn()
-    cpuId = CharColumn()
-    threadId = CharColumn()
+class TraceIdSheet(TableSheet):
+    trace_idFrom = CharColumn()
+    trace_idTo = CharColumn()
+    cpu_id = CharColumn()
+    thread_id = CharColumn()
     timestamp = IntColumn()
 
 
 class TraceWorkBook(TemplatedWorkbook):
     regular_trace_entries = TraceSheet()
-    traceid_trace_entries = TraceIdheet()
+    trace_id_trace_entries = TraceIdSheet()
 
 
 class TraceEntry():
-    def __init__(self, traceId, cpuId, threadId, timestamp, cur_prev_time_diff):
-        self.traceId = traceId
-        self.cpuId = cpuId
-        self.threadId = threadId
+    def __init__(self, trace_id, cpu_id, thread_id, timestamp, cur_prev_time_diff):
+        self.trace_id = trace_id
+        self.cpu_id = cpu_id
+        self.thread_id = thread_id
         self.timestamp = timestamp
         self.cur_prev_time_diff = cur_prev_time_diff
 
@@ -68,28 +68,28 @@ class Trace():
         previous_time = 0
         for l in self.trace:
             split_l = re.split('[\t\n]', l)
-            traceId = int(split_l[0])
-            cpuId = int(split_l[1])
-            threadId = int(split_l[2])
+            trace_id = int(split_l[0])
+            cpu_id = int(split_l[1])
+            thread_id = int(split_l[2])
             t = int(split_l[3])
-            if traceId == FIRST_TRACEID:
+            if trace_id == FIRST_trace_id:
                 previous_time = t
-            self.rows.append(TraceEntry(traceId, threadId, cpuId, t, t-previous_time))
+            self.rows.append(TraceEntry(trace_id, thread_id, cpu_id, t, t-previous_time))
             previous_time = t
 
-        self.numpy_rows = np.array([[te.traceId, te.threadId, te.cpuId, te.timestamp, te.cur_prev_time_diff] for te in self.rows])
+        self.numpy_rows = np.array([[te.trace_id, te.thread_id, te.cpu_id, te.timestamp, te.cur_prev_time_diff] for te in self.rows])
         print(self.numpy_rows)
         print("\n")
-        grouped_by_traceId = npi.group_by(self.numpy_rows[:, 0]).split(self.numpy_rows[:, :])
-        for r in grouped_by_traceId:
+        grouped_by_trace_id = npi.group_by(self.numpy_rows[:, 0]).split(self.numpy_rows[:, :])
+        for r in grouped_by_trace_id:
             print(r, "\n")
 
         y_hist = []
-        for group in grouped_by_traceId:
+        for group in grouped_by_trace_id:
             diffs = np.array([r[4] for r in group])
             y_hist.append(diffs)
 
-        trace_file_id = re.split('traces/|\.trace', self.trace.name)[1]
+        trace_file_id = re.split('traces/|[.]trace', self.trace.name)[1]
         try:
             os.mkdir('output/'+trace_file_id)
         except OSError as exc:  # Python >2.5
@@ -97,19 +97,19 @@ class Trace():
                 pass
             else:
                 raise
-        y = [[g[i][4] for i in range(len(g))] for g in grouped_by_traceId]
+        y = [[g[i][4] for i in range(len(g))] for g in grouped_by_trace_id]
         fig, ax = plt.subplots()
-        ax.plot(np.arange(len(grouped_by_traceId)), np.asarray([np.percentile(fifty, 50) for fifty in y]), label='50th percentile')
-        ax.plot(np.arange(len(grouped_by_traceId)), np.asarray([np.percentile(fourty, 40) for fourty in y]), label='40th percentile')
-        ax.plot(np.arange(len(grouped_by_traceId)), np.asarray([np.percentile(thirty, 30) for thirty in y]), label='30th percentile')
-        ax.plot(np.arange(len(grouped_by_traceId)), np.asarray([np.percentile(twenty, 20) for twenty in y]), label='20th percentile')
-        ax.plot(np.arange(len(grouped_by_traceId)), np.asarray([np.percentile(seventy, 70) for seventy in y]), label='70th percentile')
-        ax.plot(np.arange(len(grouped_by_traceId)), np.asarray([np.percentile(eighty, 80) for eighty in y]), label='80th percentile')
-        ax.plot(np.arange(len(grouped_by_traceId)), np.asarray([np.percentile(sixty, 60) for sixty in y]), label='60th percentile')
-        ax.plot(np.arange(len(grouped_by_traceId)), np.asarray([np.percentile(ten, 10) for ten in y]), label='10th percentile')
-        ax.plot(np.arange(len(grouped_by_traceId)), np.asarray([np.percentile(one, 1) for one in y]), label='1th percentile')
-        ax.plot(np.arange(len(grouped_by_traceId)), np.asarray([np.percentile(ninety, 90) for ninety in y]), label='90th percentile')
-        ax.plot(np.arange(len(grouped_by_traceId)), np.asarray([np.percentile(ninetynine, 99) for ninetynine in y]), label='99h percentile')
+        ax.plot(np.arange(len(grouped_by_trace_id)), np.asarray([np.percentile(fifty, 50) for fifty in y]), label='50th percentile')
+        ax.plot(np.arange(len(grouped_by_trace_id)), np.asarray([np.percentile(fourty, 40) for fourty in y]), label='40th percentile')
+        ax.plot(np.arange(len(grouped_by_trace_id)), np.asarray([np.percentile(thirty, 30) for thirty in y]), label='30th percentile')
+        ax.plot(np.arange(len(grouped_by_trace_id)), np.asarray([np.percentile(twenty, 20) for twenty in y]), label='20th percentile')
+        ax.plot(np.arange(len(grouped_by_trace_id)), np.asarray([np.percentile(seventy, 70) for seventy in y]), label='70th percentile')
+        ax.plot(np.arange(len(grouped_by_trace_id)), np.asarray([np.percentile(eighty, 80) for eighty in y]), label='80th percentile')
+        ax.plot(np.arange(len(grouped_by_trace_id)), np.asarray([np.percentile(sixty, 60) for sixty in y]), label='60th percentile')
+        ax.plot(np.arange(len(grouped_by_trace_id)), np.asarray([np.percentile(ten, 10) for ten in y]), label='10th percentile')
+        ax.plot(np.arange(len(grouped_by_trace_id)), np.asarray([np.percentile(one, 1) for one in y]), label='1th percentile')
+        ax.plot(np.arange(len(grouped_by_trace_id)), np.asarray([np.percentile(ninety, 90) for ninety in y]), label='90th percentile')
+        ax.plot(np.arange(len(grouped_by_trace_id)), np.asarray([np.percentile(ninetynine, 99) for ninetynine in y]), label='99h percentile')
         plt.title("Processing delay percentiles")
         plt.xlabel("Processing stage")
         plt.ylabel("Processing delay")
@@ -145,13 +145,13 @@ class Trace():
     def regular_as_xlsx(self):
         self.wb.regular_trace_entries.write(
             title="Trace",
-            objects=((te.traceId, te.cpuId, te.threadId, te.timestamp) for te in self.rows)
+            objects=((te.trace_id, te.cpu_id, te.thread_id, te.timestamp) for te in self.rows)
         )
 
         self.wb.save(self.output_fn)
 
-    def traceid_as_xlsx(self):
-        self.wb.traceid_trace_entries.write(
+    def trace_id_as_xlsx(self):
+        self.wb.trace_id_trace_entries.write(
             title="Trace ID analytics",
             objects=()
         )
@@ -169,7 +169,7 @@ class TestApp(App):
         content.bind(on_press=self.popup.dismiss)
         self.bl = BoxLayout(orientation='vertical')
 
-    def select_trace_file(self, instance):
+    def select_trace_file(self, _):
         selected_tb = None
         for c in self.bl.children:
             if isinstance(c, ToggleButton) and c.state == 'down':
@@ -180,16 +180,15 @@ class TestApp(App):
             trace = Trace(trace_file, "processed-"+trace_file.name.split(".")[0]+".xlsx")
             trace.collect_data()
             trace.regular_as_xlsx()
-            trace.traceid_as_xlsx()
+            trace.trace_id_as_xlsx()
 
         self.popup.open()
 
     def build(self):
         self.bl.add_widget(Label(text='Select trace file to analyze'))
-        pathlist = [(os.stat('../../traces/'+p.name).st_mtime, p.name) for p in Path('../../traces').glob('**/*.trace')]
-        pathlist.sort(key=lambda s: s[0])
-        for i, (time, fn) in enumerate(pathlist):
-            # print(path_in_str)
+        path_list = [(os.stat('../../traces/'+p.name).st_mtime, p.name) for p in Path('../../traces').glob('**/*.trace')]
+        path_list.sort(key=lambda s: s[0])
+        for i, (time, fn) in enumerate(path_list):
             if i == 0:
                 self.bl.add_widget(ToggleButton(text=fn, group="trace file", state='down'))
             else:
